cd22f14f52b41763d129ff4e954e4520fafcbac3
danielpancake <dsatarov@outlook.com>
2021-08-04 09:40:32 +0300

    Chrono lib

--- InnoBootcamp/1 - Iterative and recursive/1.cpp
+++ InnoBootcamp/1 - Iterative and recursive/1.cpp
@@ -1,9 +1,10 @@
+#include <chrono>
 #include <iostream>
 
-int Fib(int n) {
+long long int Fib(int n) {
     if (n <= 0) return 0;
 
-    int a = 1, b = 1;
+    long long int a = 1, b = 1;
     for (int i = 0; i < n - 1; i++) {
         b = a + b;
         a = b - a;
@@ -12,14 +13,37 @@ int Fib(int n) {
     return a;
 }
 
-int FibRecursive(int n) {
+long long int FibRecursive(int n) {
     if (n < 1) return 0;
     if (n < 2) return 1;
     return FibRecursive(n - 2) + FibRecursive(n - 1);
 }
 
-int main() {
-    int N; std::cin >> N;
+std::chrono::steady_clock::time_point getNow() {
+    return std::chrono::steady_clock::now();
+}
 
-    std::cout << Fib(N) << "   " << FibRecursive(N);
+double getTime(std::chrono::steady_clock::time_point start, std::chrono::steady_clock::time_point end) {
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+}
+
+int main() {
+    int N, t; std::cin >> N;
+    std::chrono::steady_clock::time_point start, end;
+
+    start = getNow();
+    std::cout << Fib(N) << " ";
+    end = getNow();
+
+    t = getTime(start, end);
+
+    std::cout << "Iterative exec. time: " << t << "μs" << std::endl;
+
+    start = getNow();
+    std::cout << FibRecursive(N) << " ";
+    end = getNow();
+
+    t = getTime(start, end);
+
+    std::cout << "Recursive exec. time: " << t << "μs" << std::endl;
 }
